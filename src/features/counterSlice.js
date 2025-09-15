2737b9d5ff2e8ccd6989c0336709b86b845f0c30
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-09-15 02:35:15 -0700

    createSlice, name initialState, reducers, reducer

---
+++ src/features/counterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+    name:"counter",
+    initialState:{ value:0 },
+    reducers: {
+        addToy: ()=>{
+state.value += 1;
+        },
+        removeToy: ()=>{
+state.value -= 1;
+        },
+    },
+});
+
+export const { addToy, removeToy } = counterSlice.actions;
+export default counterSlice.reducer;
+
+
+// reducers are the actions => the rules that defines how to change the box 
+// reducer is the brain that do the work and reducers are the work the action that reducer does addToy, removeToy are actions that are define reducers. 
+// reducer is the brain the worker the machine that follow those rules, action and do change the box.
