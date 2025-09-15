19f5e0022483c2e3b5336bbe30a9f1ef2101115e
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-09-15 02:38:51 -0700

    Update counterSlice.js
    When you call createSlice, it gives you an object with:
    
    slice.actions → ready-made action creators
    
    slice.reducer → the reducer function you put inside your store

--- src/features/counterSlice.js
+++ src/features/counterSlice.js
@@ -17,6 +17,6 @@ export const { addToy, removeToy } = counterSlice.actions;
 export default counterSlice.reducer;
 
 
-// reducers are the actions => the rules that defines how to change the box 
+// reducers are the actions => the rules that defines how to change the box , the rules inside a rulbook
 // reducer is the brain that do the work and reducers are the work the action that reducer does addToy, removeToy are actions that are define reducers. 
-// reducer is the brain the worker the machine that follow those rules, action and do change the box.
+// reducer is the brain the worker the machine that follow those rules, action and do change the box., is the rule book
